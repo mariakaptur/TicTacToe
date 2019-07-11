6e7a994b24fc24ee393cd937a9ccec8e1cbc68f7
Maria Kaptur <33967426+mariakaptur@users.noreply.github.com>
2019-07-11 13:58:48 +0200

    No click after a win added

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+var cells = document.querySelectorAll('.cell'),
+    X_or_O = 0;
+var turn = document.getElementById("turn");
+
+turn.innerHTML = 'Play with a friend';
+
+playGame();
+
 function getWinner() {
     if (c1.innerHTML !== "" && c1.innerHTML === c2.innerHTML && c1.innerHTML === c3.innerHTML)
         selectWinnerCells(c1, c2, c3);
@@ -15,10 +23,25 @@ function getWinner() {
         selectWinnerCells(c7, c8, c9);
 }
 
-var cells = document.querySelectorAll('.cell'),
-    X_or_O = 0;
-var turn = document.getElementById("turn");
-turn.innerHTML = 'Play with a friend';
+function playGame() {
+    for (var i = 0; i < cells.length; i++) {
+        cells[i].onclick = function () {
+            if (this.innerHTML !== "X" && this.innerHTML !== "O") {
+                if (X_or_O % 2 === 0) {
+                    this.innerHTML = "X";
+                    turn.innerHTML = "Your turn : O";
+                    getWinner();
+                    X_or_O += 1;
+                } else {
+                    this.innerHTML = "O";
+                    turn.innerHTML = "Your turn : X";
+                    getWinner();
+                    X_or_O += 1;
+                }
+            }
+        }
+    };
+}
 
 function selectWinnerCells(winCell1, winCell2, winCell3) {
     winCell1.classList.add("win");
@@ -26,26 +49,10 @@ function selectWinnerCells(winCell1, winCell2, winCell3) {
     winCell3.classList.add("win");
     turn.innerHTML = winCell1.innerHTML + " player won";
     turn.style.fontWeight = "bold";
-}
-
-for (var i = 0; i < cells.length; i++) {
-    cells[i].onclick = function () {
-
-        if (this.innerHTML !== "X" && this.innerHTML !== "O") {
-            if (X_or_O % 2 === 0) {
-                this.innerHTML = "X";
-                turn.innerHTML = "Your turn : O";
-                getWinner();
-                X_or_O += 1;
-            } else {
-                this.innerHTML = "O";
-                turn.innerHTML = "Your turn : X";
-                getWinner();
-                X_or_O += 1;
-            }
-        }
+    for (var i = 0; i < cells.length; i++) {
+        this.cells[i].onclick = null;
     }
-};
+}
 
 function replay() {
     for (i = 0; i < cells.length; i++) {
@@ -53,9 +60,9 @@ function replay() {
         turn.innerHTML = 'Play with a friend';
         turn.style.fontWeight = "normal";
         cells[i].classList.remove('win');
+        playGame();
     }
 }
-
 
 document.addEventListener("DOMContentLoaded", function (event) {
     var c1 = document.getElementById('c1');

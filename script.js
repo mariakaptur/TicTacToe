df14ff8e6c54ec068bf4729517175b8493adf0cd
Maria Kaptur <33967426+mariakaptur@users.noreply.github.com>
2019-07-11 08:20:32 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-var gameBoard;
+console.log('js ready')
+let gameBoard;
 const huPlayer = "X";
 const aiPlayer = "O";
-const boxes = document.querySelectorAll('.box');
+const cells = document.querySelectorAll('.cell');
+const winCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8]
+];
+
 
 startGame();
 
 function startGame() {
     gameBoard = Array.from(Array(9).keys());
-    for (i = 0; i < boxes.length; i++) {
-        boxes[i].innerText = '';
-        boxes[i].addEventListener('click', turnClick, false);
+    for (i = 0; i < cells.length; i++) {
+        cells[i].innerText = '';
+        cells[i].style.removeProperty('background-color');
+        cells[i].addEventListener('click', turnClick, false);
     }
 }
 
@@ -17,7 +30,31 @@ function turnClick(square) {
     turn(square.target.id, huPlayer);
 }
 
-function turn(squareID, player) {
-    gameBoard[squareID] = player;
-    document.getElementById(squareID).innerText = player;
+function turn(squareId, player) {
+    gameBoard[squareId] = player;
+    document.getElementById(squareId).innerText = player;
+    let gameWon = checkWin(gameBoard, player);
+    if(gameWon) gameOver(gameWon);
+}
+
+function checkWin(board, player){
+    let currentPlay = board.reduce((a, e, i) => e === player ? a.concat(i) : a, []);
+    let gameWon = null;
+    for(let [index, win] of winCombos.entries()) {
+        if (win.every(elem => currentPlay.indexOf(elem) > -1)) {
+            gameWon = {index: index, player: player};
+            break;
+        }
+    }
+    return gameWon;
+}
+
+function gameOver(gameWon){
+    for (let index of winCombos[gameWon.index]) {
+        document.getElementById(index).style.backgroundColor =
+        gameWon.player == huPlayer ? "blue" : "red";
+    } console.log('blue')
+    for (let i=0; i < cells.length; i++){
+        cells[i].removeEventListener('click', turnClick, false);
+    }
 }

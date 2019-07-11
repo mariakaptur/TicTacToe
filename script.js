0afe2dd2a707bf0ea031da9a23827e7e59d05cfb
Maria Kaptur <33967426+mariakaptur@users.noreply.github.com>
2019-07-11 13:38:44 +0200

    New commit

--- script.js
+++ script.js
@@ -1,60 +1,72 @@
-console.log('js ready')
-let gameBoard;
-const huPlayer = "X";
-const aiPlayer = "O";
-const cells = document.querySelectorAll('.cell');
-const winCombos = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8]
-];
-
-
-startGame();
-
-function startGame() {
-    gameBoard = Array.from(Array(9).keys());
-    for (i = 0; i < cells.length; i++) {
-        cells[i].innerText = '';
-        cells[i].style.removeProperty('background-color');
-        cells[i].addEventListener('click', turnClick, false);
-    }
+function getWinner() {
+    if (c1.innerHTML !== "" && c1.innerHTML === c2.innerHTML && c1.innerHTML === c3.innerHTML)
+        selectWinnerCells(c1, c2, c3);
+    if (c1.innerHTML !== "" && c1.innerHTML === c4.innerHTML && c1.innerHTML === c7.innerHTML)
+        selectWinnerCells(c1, c4, c7);
+    if (c1.innerHTML !== "" && c1.innerHTML === c5.innerHTML && c1.innerHTML === c9.innerHTML)
+        selectWinnerCells(c1, c5, c9);
+    if (c2.innerHTML !== "" && c2.innerHTML === c5.innerHTML && c2.innerHTML === c8.innerHTML)
+        selectWinnerCells(c2, c5, c8);
+    if (c3.innerHTML !== "" && c3.innerHTML === c6.innerHTML && c3.innerHTML === c9.innerHTML)
+        selectWinnerCells(c3, c6, c9);
+    if (c3.innerHTML !== "" && c3.innerHTML === c5.innerHTML && c3.innerHTML === c7.innerHTML)
+        selectWinnerCells(c3, c5, c7);
+    if (c7.innerHTML !== "" && c7.innerHTML === c8.innerHTML && c7.innerHTML === c9.innerHTML)
+        selectWinnerCells(c7, c8, c9);
 }
 
-function turnClick(square) {
-    turn(square.target.id, huPlayer);
+var cells = document.querySelectorAll('.cell'),
+    X_or_O = 0;
+var turn = document.getElementById("turn");
+turn.innerHTML = 'Play with a friend';
+
+function selectWinnerCells(winCell1, winCell2, winCell3) {
+    winCell1.classList.add("win");
+    winCell2.classList.add("win");
+    winCell3.classList.add("win");
+    turn.innerHTML = winCell1.innerHTML + " player won";
+    turn.style.fontWeight = "bold";
 }
 
-function turn(squareId, player) {
-    gameBoard[squareId] = player;
-    document.getElementById(squareId).innerText = player;
-    let gameWon = checkWin(gameBoard, player);
-    if(gameWon) gameOver(gameWon);
-}
+for (var i = 0; i < cells.length; i++) {
+    cells[i].onclick = function () {
 
-function checkWin(board, player){
-    let currentPlay = board.reduce((a, e, i) => e === player ? a.concat(i) : a, []);
-    let gameWon = null;
-    for(let [index, win] of winCombos.entries()) {
-        if (win.every(elem => currentPlay.indexOf(elem) > -1)) {
-            gameWon = {index: index, player: player};
-            break;
+        if (this.innerHTML !== "X" && this.innerHTML !== "O") {
+            if (X_or_O % 2 === 0) {
+                this.innerHTML = "X";
+                turn.innerHTML = "Your turn : O";
+                getWinner();
+                X_or_O += 1;
+            } else {
+                this.innerHTML = "O";
+                turn.innerHTML = "Your turn : X";
+                getWinner();
+                X_or_O += 1;
+            }
         }
     }
-    return gameWon;
-}
+};
 
-function gameOver(gameWon){
-    for (let index of winCombos[gameWon.index]) {
-        document.getElementById(index).style.backgroundColor =
-        gameWon.player == huPlayer ? "blue" : "red";
-    } console.log('blue')
-    for (let i=0; i < cells.length; i++){
-        cells[i].removeEventListener('click', turnClick, false);
+function replay() {
+    for (i = 0; i < cells.length; i++) {
+        cells[i].innerText = '';
+        turn.innerHTML = 'Play with a friend';
+        turn.style.fontWeight = "normal";
+        cells[i].classList.remove('win');
     }
 }
+
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    var c1 = document.getElementById('c1');
+    var c2 = document.getElementById('c2');
+    var c3 = document.getElementById('c3');
+    var c4 = document.getElementById('c4');
+    var c5 = document.getElementById('c5');
+    var c6 = document.getElementById('c6');
+    var c7 = document.getElementById('c7');
+    var c8 = document.getElementById('c8');
+    var c9 = document.getElementById('c9');
+
+    var turn = document.getElementById("turn");
+});
